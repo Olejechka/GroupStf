b199bb1b12318dfdb17db8e9e2cd5e591bc7a1e6
FreddyFazbear-83 <144786534+FreddyFazbear-83@users.noreply.github.com>
2025-11-10 16:06:08 +0300

    add background

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+
+from PyQt5.QtCore import QSize
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
 
 
 class Mashinkov(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("бандит")
+        self.setWindowTitle("Бандит.exe")
 
         screen = QApplication.primaryScreen().size()
         window_width = int(screen.width() * 0.8)
         window_height = int(screen.height() * 0.6)
+
+        self.background_label = QLabel(self)
+        self.background_label.setGeometry(0, 0, window_width, window_height)
+        self.background_label.setScaledContents(True)
+        self.background_image = QPixmap("mat/maxresdefault.jpg")
+        self.background_label.setPixmap(self.background_image)
 
         self.setFixedSize(window_width, window_height)
 
