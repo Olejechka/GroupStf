f7b61c619ffc42805f4a6ee221049209b1e34dd2
FreddyFazbear-83 <144786534+FreddyFazbear-83@users.noreply.github.com>
2025-11-16 17:52:55 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,40 @@
 import sys
-
-from PyQt5.QtCore import QSize
-from PyQt5.QtGui import QIcon, QPixmap
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
-
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaPlaylist, QMediaContent
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QMessageBox,QSlider
+from PyQt5.QtCore import Qt, QTimer, QRect, QSize,QUrl
+from PyQt5.QtGui import QPainter, QPixmap, QFont,QIcon,QMovie
+from PyQt5 import QtWidgets
 
 class Mashinkov(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Бандит.exe")
 
+        self.playlist = QMediaPlaylist()
+        self.playlist.addMedia(QMediaContent(QUrl.fromLocalFile("mat/Blue_Moon.mp3")))
+        self.playlist.setPlaybackMode(QMediaPlaylist.Loop)
+        #
+        self.music_player = QMediaPlayer()
+        self.music_player.setPlaylist(self.playlist)
+        self.music_player.play()
+
+        def play_music(self):
+            self.music_player.play()
+
         screen = QApplication.primaryScreen().size()
-        window_width = int(screen.width() * 0.8)
-        window_height = int(screen.height() * 0.6)
+        window_width = int(screen.width() * 0.7)
+        window_height = int(screen.height() * 0.7)
 
         self.background_label = QLabel(self)
         self.background_label.setGeometry(0, 0, window_width, window_height)
         self.background_label.setScaledContents(True)
-        self.background_image = QPixmap("mat/maxresdefault.jpg")
+        self.background_image = QPixmap("mat/new2.png")
+        self.background_label.setPixmap(self.background_image)
+
+        self.background_label = QLabel(self)
+        self.background_label.setGeometry(300, 0, 700, 790)
+        self.background_label.setScaledContents(True)
+        self.background_image = QPixmap("mat/bb.png")
         self.background_label.setPixmap(self.background_image)
 
         self.setFixedSize(window_width, window_height)
